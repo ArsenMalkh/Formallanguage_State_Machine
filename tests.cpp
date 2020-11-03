18d5bbb1eedeeb43af71f70eb1f8cd0e5d0c3a2d
ArsenMalkh <61829741+ArsenMalkh@users.noreply.github.com>
2020-11-03 23:32:38 +0400

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -15,49 +15,6 @@ TEST(general_tests, second) {
     EXPECT_EQ(parser.GetMaxSubstrLength(), 2);
 }
 
-TEST(general_tests, third) {
-    std::string u= "hoqeirjkabcbbbabaaabbabababcbbababddfdwwerdsfasaaababacasnnb";
-    std::string regexp = "ab+*c.ab+*.";
-    Regexp parser(u, regexp);
-    EXPECT_EQ(parser.GetMaxSubstring(), "bbbabaaabbabababcbbabab");
-}
-
-TEST(general_tests, fotrh) {
-    std::string u= "nonsence";
-    std::string regexp = "ab+*c.ab+*.";
-    Regexp parser(u, regexp);
-    EXPECT_EQ(parser.GetCommonRegexp(), "((a+b))*c((a+b))*");
-}
-
-TEST(general_tests, fifth) {
-    std::string u= "otioirweoirewoirewo";
-    std::string regexp = "ab+*c.ab+*.";
-    Regexp parser(u, regexp);
-    std::cout << parser.GetMaxSubstring() << "\n";
-    EXPECT_EQ(parser.GetMaxSubstring(), "");
-}
-
-TEST(error_tests, first) {
-    std::string u= "abaidgaibac";
-    std::string regexp = "+ab";
-    Regexp parser(u, regexp);
-    EXPECT_EQ(parser.GetCommonRegexp(), "");
-}
-
-TEST(error_tests, second) {
-    std::string u= "aidafoasd";
-    std::string regexp = "a.b";
-    Regexp parser(u, regexp);
-    EXPECT_EQ(parser.GetMaxSubstring(), "Incorrect input: regexp is wrong");
-}
-
-TEST(error_tests, third) {
-    std::string u= "ab+*c.ab+*.abc";
-    std::string regexp = "a.b";
-    Regexp parser(u, regexp);
-    EXPECT_EQ(parser.GetMaxSubstrLength(), std::numeric_limits<int32_t>::max());
-}
-
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
