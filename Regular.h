1a1fe403c173866479d7781b190c1c0e2ccea8f2
ArsenMalkh <61829741+ArsenMalkh@users.noreply.github.com>
2020-11-03 22:51:35 +0400

    Create Regular.h

---
+++ Regular.h
@@ -0,0 +1,24 @@
+#progma once
+#include <iostream>
+#include <unordered_set>
+#include <set>
+#include <stack>
+#include <string>
+#include <cstdint>
+
+class Regexp {
+private:
+    void Create();    
+	  std::stack<std::string> regular_exp;
+    std::stack<std::set<std::pair<uint32_t, uint32_t >>> substr_sets;
+    const std::string str;
+    const std::string reg_rpn;
+    std::unordered_set<char> symbols;
+    bool is_incorrect;
+
+public:
+    Regexp(const std::string &str, const std::string& rpn);
+    int GetMaxSubstrLength() const;
+	  bool GetCorrection() const;
+
+};
