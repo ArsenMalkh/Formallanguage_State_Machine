8f5447e9331356d382ac4526fefc01c235e28713
ArsenMalkh <61829741+ArsenMalkh@users.noreply.github.com>
2020-11-04 00:09:33 +0400

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,20 +1,46 @@
 #include "Regular.h"
 #include <gtest/gtest.h>
 
-TEST(general_tests, first) {
+TEST(general_test, first) {
+    std::string u = "abbbacbaa";
+    std::string regexp = "ab+c+c.ab+.";
+    Regexp parser(u, regexp);
+    EXPECT_EQ(parser.GetMaxSubstrLength(), 3);
+}
+
+TEST(general_test, second) {
+    std::string u= "ababacbababbcaaa";
+    std::string regexp = "ac+b.*ab+c..";
+    Regexp parser(u, regexp);
+    EXPECT_EQ(parser.GetMaxSubstrLength(), 2);
+}
+
+TEST(general_test, third) {
     std::string u= "abbaa";
     std::string regexp = "acb..bab.c.*.ab.ba.+.+*a.";
     Regexp parser(u, regexp);
     EXPECT_EQ(parser.GetMaxSubstrLength(), 4);
 }
 
-TEST(general_tests, second) {
+TEST(general_tests, fourth) {
     std::string u= "babc";
     std::string regexp = "ab+c.aba.*.bac.+.*+*";
     Regexp parser(u, regexp);
     EXPECT_EQ(parser.GetMaxSubstrLength(), 2);
 }
+TEST(error_test, first) {
+    std::string u= "abbababbabaa";
+    std::string regexp = "acb..bab.c.*.ab.ba.+.+*a";
+    Regexp parser(u, regexp);
+    EXPECT_EQ(parser.GetMaxSubstrLength(), "ERROR");
+}
 
+TEST(general_tests, second) {
+    std::string u= "bbcababaa";
+    std::string regexp = "ab+cc..a.bb";
+    Regexp parser(u, regexp);
+    EXPECT_EQ(parser.GetMaxSubstrLength(), "ERROR");
+}
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
