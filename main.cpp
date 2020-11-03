fcd43ef90a53bfcb5974cc8a3ba35cd963aa3519
ArsenMalkh <61829741+ArsenMalkh@users.noreply.github.com>
2020-11-03 23:06:21 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Regular.h>
+
+int main() {
+    std::string u;
+    std::string regexp;
+    std::cin>>regexp>>u;
+    Regexp parser(u, regexp);
+	if(!parser.GetCorrection())
+    std::cout<<parser.GetMaxSubstrLength();
+	else
+		std::cout<<"ERROR"<<std::endl;
+	system("pause");
+	return 0;
+}
